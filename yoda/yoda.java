cfbd06730924c9584145efec723531b22c2f7dec
Balázs Tóth <gawnoris@gmail.com>
2019-09-11 14:49:07 +0200

    Add files via upload

---
+++ yoda/yoda.java
@@ -0,0 +1,22 @@
+public class yoda
+{
+	String i;
+   public yoda()
+   {
+   i=null;
+		
+   }
+   public static void main(String[] args)
+	{
+	yoda f=new yoda();
+
+	if(f.i.equals("5"))
+	{
+	System.out.println("t");
+	}
+	else
+	{
+	System.out.println("f");
+	}
+   }
+}
