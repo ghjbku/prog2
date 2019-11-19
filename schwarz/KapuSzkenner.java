631cd72ba01125ad9c47af9d6146754db066b40c
windsake <gawnoris@gmail.com>
2019-11-19 22:25:20 +0100

    schwarz1

--- schwarz/KapuSzkenner.java
+++ schwarz/KapuSzkenner.java
@@ -2,13 +2,13 @@ public class KapuSzkenner {
     
     public static void main(String[] args) {
         
-        for(int i=0; i<300; ++i)
+        for(int i=0; i<1024; ++i)
             
             try {
                 //String s=null;
                 java.net.Socket socket = new java.net.Socket(args[0], i);
                 
-                System.out.println(i + " figyeli");
+                System.out.println(i + " igen figyeli");
                 
                 socket.close();
                 
